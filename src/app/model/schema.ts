514795d77e9686d9429b56518b4bb9ea42164582
Mack_Ignacio <mack_ignacio@yahoo.com>
2018-07-11 17:43:20 +0800

    Add data interface

--- src/app/model/schema.ts
+++ src/app/model/schema.ts
@@ -1,84 +1,89 @@
-export class Item{
-    name:string;
-    price:string;
-    itemcode:string;
-    stocks:string;
-    brand:string;
-    model:string;
-    description:string;
+export class Item {
+  name: string;
+  price: string;
+  itemcode: string;
+  stocks: string;
+  brand: string;
+  model: string;
+  description: string;
 }
-export class User{
-    id:string;
-    username:string;
-    password:string;
-    isLogin:string;
-    loginExpired:string;
-    birthday:string;
-    position:string;
-    usertype:string;
-    employeeName:string;
-}
-
-export class Delivery{
-    item:string;
-    quantity:string;
-    deliveryDate:string;
-    refNo:string;
-    supplier:string;
-    batch:string;
-    serialNo:string;
-    chasis:string;
-    engine:string;
+export class User {
+  id: string;
+  username: string;
+  password: string;
+  isLogin: string;
+  loginExpired: string;
+  birthday: string;
+  position: string;
+  usertype: string;
+  employeeName: string;
 }
 
-export class Purchase{
-    client:string;
-    agent:string;
-    date: string;
-    items:{
-        itemID:string;
-        quantity:string;
-        price:string;
-        total:string;
-    }
+export class Delivery {
+  item: string;
+  quantity: string;
+  deliveryDate: string;
+  refNo: string;
+  supplier: string;
+  batch: string;
+  serialNo: string;
+  chasis: string;
+  engine: string;
 }
 
-export class Client{
-    
-    clientName:String;
-    clientAddress:String;
-    clientContact:String;
-    clientAge:String;
-    clientEmail:String;
-    clientBirthday:String;
+export class Purchase {
+  client: string;
+  agent: string;
+  date: string;
+  items: {
+    itemID: string;
+    quantity: string;
+    price: string;
+    total: string;
+  };
 }
 
-export class Agent{
-    
-        agentName:String;
-        agentAddress:String;
-        agentContact:String;
-        agentAge:String;
-        agentEmail:String;
-        agentBirthday:String;
+export class Client {
+  clientName: String;
+  clientAddress: String;
+  clientContact: String;
+  clientAge: String;
+  clientEmail: String;
+  clientBirthday: String;
 }
 
-export class PackingList{
-    company:String;
-    attention:String;
-    date:String;
-    plNo:String;
-    ref:String;
-    blNo:String;
-    loading:String;
-    discharge:String;
-    shipping:String;
-    partial:String;
-    insurance:String;
-    weight:String;
-    quantity:String;
-    measurement:String;
-    description:String;
-    totalweight:String;
-    totalmeasurement:String;
+export class Agent {
+  agentName: String;
+  agentAddress: String;
+  agentContact: String;
+  agentAge: String;
+  agentEmail: String;
+  agentBirthday: String;
 }
+
+export class PackingList {
+  company: String;
+  attention: String;
+  date: String;
+  plNo: String;
+  ref: String;
+  blNo: String;
+  loading: String;
+  discharge: String;
+  shipping: String;
+  partial: String;
+  insurance: String;
+  weight: String;
+  quantity: String;
+  measurement: String;
+  description: String;
+  totalweight: String;
+  totalmeasurement: String;
+}
+
+export interface Data {
+  message: string;
+  status: string;
+  id: string;
+}
+
