bd4b65dfd5f193c6601f428b6456161431df4a06
Mack_Ignacio <mack_ignacio@yahoo.com>
2018-07-11 17:42:45 +0800

    Update to rxjs 6 syntax

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,41 +1,43 @@
-import { Injectable } from '@angular/core';
-import {CanActivate,CanActivateChild, Router, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
-import { DatabaseService } from '../services/database.service';
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  CanActivateChild,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from "@angular/router";
+import { pipe } from "rxjs";
+import { map } from "rxjs/operators";
+import { DatabaseService } from "../services/database.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate, CanActivateChild {
+  redirectURL;
+  data;
 
-    redirectURL;
-    data;
-    
-    constructor(
-        private dbService: DatabaseService,
-        private router: Router
-      ) {}
+  constructor(private dbService: DatabaseService, private router: Router) {}
 
-  canActivate(router : ActivatedRouteSnapshot,state : RouterStateSnapshot){
-    return this.data = this.dbService.loggedIn().map(data => { 
-      
-      if (data == true){
+  canActivate(router: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return (this.data = this.dbService.loggedIn().pipe(map(data => {
+      if (data === true) {
         return true;
       } else {
         localStorage.clear();
-        this.router.navigate(['']);
+        this.router.navigate([""]);
         return false;
       }
-    });
+    })));
   }
 
-  canActivateChild(router : ActivatedRouteSnapshot,state : RouterStateSnapshot){
-    return this.data = this.dbService.loggedIn().map(data => { 
-      
-      if (data == true){
+  canActivateChild(router: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return (this.data = this.dbService.loggedIn().pipe(map(data => {
+      if (data === true) {
         return true;
       } else {
         localStorage.clear();
-        this.router.navigate(['']);
+        this.router.navigate([""]);
         return false;
       }
-    });
+    })));
   }
 }
--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,50 +1,65 @@
-import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
-import 'rxjs/add/operator/map';
-import { environment as envi } from '../../environments/environment';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { environment as envi } from "../../environments/environment";
 
 @Injectable()
 export class DatabaseService {
-  token = localStorage.getItem('token');
+  token = localStorage.getItem("token");
   conn = envi.url;
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) {}
+
+  private handleError(error: HttpErrorResponse) {
+    return throwError(error);
+  }
 
   get(fun, id?) {
-    this.token = localStorage.getItem('token');
-    const url = id !== undefined ? this.conn + '?function=' + fun + '&token=' + this.token + '&id=' + id :
-      this.conn + '?function=' + fun + '&token=' + this.token;
-    return this.http.get(url).map(res => this.check(res), err => err.json());
+    this.token = localStorage.getItem("token");
+    const url =
+      id !== undefined
+        ? this.conn + "?function=" + fun + "&token=" + this.token + "&id=" + id
+        : this.conn + "?function=" + fun + "&token=" + this.token;
+    return this.http.get(url).pipe(catchError(this.handleError));
   }
 
   post(data) {
-    return this.http.post(this.conn, { data: data, token: this.token }).map(res => this.check(res), err => err.json());
+    return this.http
+      .post(this.conn, { data: data, token: this.token })
+      .pipe(catchError(this.handleError));
   }
 
   getPDF(val?) {
     if (val !== null || val !== undefined) {
-      return this.http.get('./assets/pdf/' + val + '.json').map(res => res.json());
+      return this.http
+        .get("./assets/pdf/" + val + ".json")
+        .pipe(catchError(this.handleError));
     }
-    return this.http.get('./assets/pdf/pdf.json').map(res => res.json());
+    return this.http
+      .get("./assets/pdf/pdf.json")
+      .pipe(catchError(this.handleError));
   }
 
   loggedIn() {
     const data = {
-      function: 'checkLogin',
-      User: localStorage.getItem('user'),
-      Token: localStorage.getItem('token')
+      function: "checkLogin",
+      User: localStorage.getItem("user"),
+      Token: localStorage.getItem("token")
     };
 
-    return this.http.post(this.conn, { data: data, token: localStorage.getItem('token') }).map(res => this.check(res), err => err.json());
+    return this.http
+      .post(this.conn, { data: data, token: localStorage.getItem("token") })
+      .pipe(catchError(this.handleError));
   }
 
   userLogin(data) {
-    return this.http.post(this.conn, { data: data }).map(res => this.check(res), err => err.json());
+    return this.http
+      .post(this.conn, { data: data })
+      .pipe(catchError(this.handleError));
   }
 
   check(res) {
-    res !== null ? null : console.log(res);
     return res !== null ? res.json() : {};
   }
-
 }
